38b5e1f2140f5fa23e3f1a3ca7f4b4bf151de042
Robin Reyes <robinreyes.code@gmail.com>
2023-03-22 12:24:37 -0700

    mobile complete

---
+++ main.js
@@ -0,0 +1,20 @@
+// const sliderImages = document.querySelectorAll(".slider-image");
+// let currentImage = 0;
+
+// function reset() {
+//   sliderImages.forEach((image) => {
+//     image.classList.remove("active");
+//   });
+// }
+
+// function startSlide() {
+//   reset();
+//   sliderImages[currentImage].classList.add("active");
+//   currentImage++;
+//   if (currentImage === sliderImages.length) {
+//     currentImage = 0;
+//   }
+//   setTimeout(startSlide, 5000);
+// }
+
+// startSlide();
